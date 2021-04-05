c112b3191c65aded1a114ebc9979ac8b517134a9
Guillermo Aguilar <gaguilar128@gmail.com>
2021-04-04 19:47:48 -0500

    storybook-app

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
@@ -20,13 +21,44 @@ connectDB()
 
 const app = express()
 
+//Body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+
+//Method override
+app.use(methodOverride(function(req, res){
+  if(req.body && typeof req.body === 'object' && '_method' in req.body){
+    let method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+
+
 //Logging
 if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
+//hadlebars helpers
+const { formatDate, stripTags, truncate, editIcon, select} = require('./helpers/hbs')
+
 //handlebars
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}));
+app.engine(
+  '.hbs',
+  exphbs({
+    helpers: {
+      formatDate,
+      stripTags,
+      truncate,
+      editIcon,
+      select
+    },
+    defaultLayout: 'main',
+    extname: '.hbs'
+  })
+)
 app.set('view engine', '.hbs')
 
 //Sessions
@@ -42,6 +74,13 @@ app.use(
 //Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// Set global variable
+app.use(function(req, res, next){
+  res.locals.user = req.user || null
+  next()
+})
+
 
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')))
